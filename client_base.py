6a94c3006ffd3170432baa5a8d758d415288fe96
Eddyme34 <49800956+Eddyme34@users.noreply.github.com>
2020-08-23 18:42:36 -0700

    changed

---
+++ client_base.py
@@ -0,0 +1,68 @@
+import socket
+import argparse
+import threading
+
+parser = argparse.ArgumentParser(description = "This is the client for the multi threaded socket server!")
+parser.add_argument('--host', metavar = 'host', type = str, nargs = '?', default = '192.168.1.92')
+parser.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 9999)
+args = parser.parse_args()
+
+print(f"Connecting to server: {args.host} on port: {args.port}")
+
+sck_msg = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sck_vid = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+try:
+    sck_msg.connect((args.host, args.port))
+    sck_vid.connect((args.host, 1234))
+except Exception as e:
+    raise SystemExit(f"We have failed to connect to host: {args.host} on port: {args.port}, because: {e}")
+
+
+def msg_conn():
+    while True:
+        msg = input("What do we want to send to the server?: ")
+        sck_msg.sendall(msg.encode('utf-8'))
+        if msg =='exit':
+            print("Client is saying goodbye!")
+            break
+        data = sck_msg.recv(1024)
+        print(f"The server's response was: {data.decode()}")
+    client.close()
+    
+
+def video_conn():
+    payload_size = struct.calcsize("<L")
+    data = b''
+    while(True):
+        start_time = datetime.datetime.now()
+        while len(data) < payload_size:
+            data += sck_vid.recv(4096)
+        frame_size = struct.unpack("<L", data[:payload_size])[0]
+        data = data[payload_size:]
+        while len(data) < frame_size:
+            data += sck_vid.recv(131072)
+        frame_data = data[:frame_size]
+        data = data[frame_size:]
+        img = base64.b64decode(frame_data)
+        npimg = np.frombuffer(img, dtype=np.uint8)
+        frame = cv2.imdecode(npimg, 1)
+
+        #frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        end_time = datetime.datetime.now()
+        fps = 1/(end_time-start_time).total_seconds()
+        print("Fps: ",round(fps,2))
+
+        # Display
+        cv2.imshow('frame', frame)
+        
+        if (cv2.waitKey(1) & 0xFF == ord('q')):
+            cv2.destroyAllWindows()
+            break;
+
+
+threading._start_new_thread(msg_conn, ())
+threading._start_new_thread(video_conn, ())
+    
+
---
+++ server_rover.py
@@ -0,0 +1,67 @@
+import socket
+import argparse
+import threading
+
+parser = argparse.ArgumentParser(description = "This is the server for the multithreaded socket demo!")
+parser.add_argument('--host', metavar = 'host', type = str, nargs = '?', default = '192.168.1.92')
+parser.add_argument('--port', metavar = 'port', type = int, nargs = '?', default = 9999)
+args = parser.parse_args()
+
+print(f"Running the server on: {args.host} and port: {args.port}")
+
+sck_msg = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sck_vid = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    sck_msg.bind((args.host, args.port))
+    sck_msg.listen(5)
+    sck_vid.bind((args.host, 1234))
+    sck_vid.listen(5)
+except Exception as e:
+    raise SystemExit(f"We could not bind the server on host: {args.host} to port: {args.port}, because: {e}")
+
+
+def on_new_client(client, connection):
+    ip = connection[0]
+    port = connection[1]
+    print(f"THe new connection was made from IP: {ip}, and port: {port}!")
+    while True:
+        msg = client.recv(1024)
+        if msg.decode() == 'exit':
+            break
+        print(f"The client said: {msg.decode()}")
+        reply = f"You told me: {msg.decode()}"
+        client.sendall(reply.encode('utf-8'))
+    print(f"The client from ip: {ip}, and port: {port}, has gracefully diconnected!")
+    client.close()
+    
+def video_stream(client, connection):
+    ip = connection[0]
+    port = connection[1]
+    print(f"THe new connection was made from IP: {ip}, and port: {port}!")
+    while True:
+        ret,frame=cap.read()
+        if (ret):
+            encoded, buffer = cv2.imencode('.jpg', frame)
+            b_frame = base64.b64encode(buffer)
+            b_size = len(b_frame)
+            #sending data
+            client.sendall(struct.pack("<L", b_size) + b_frame)
+    print(f"The client from ip: {ip}, and port: {port}, has gracefully diconnected!")
+    client.close()
+
+while True:
+    try:
+        client, ip = sck_msg.accept()
+        client2, ip2 = sck_vid.accept()
+        threading._start_new_thread(on_new_client,(client, ip))
+        #threading._start_new_thread(video_stream,(client2, ip2))
+        
+        
+    except KeyboardInterrupt:
+        print(f"shutting down the server!")
+        break
+    except Exception as e:
+        print(f"error: {e}")
+
+sck_msg.close()
